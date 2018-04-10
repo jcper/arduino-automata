8bf8c8dbfe8a0946fb6e25bdf7ff7e551ff5d3b4
jcper <you@example.com>
2018-04-11 00:53:56 +0200

    envio request cada 10 segundos

--- WebClientheroku/WebClientheroku.ino
+++ WebClientheroku/WebClientheroku.ino
@@ -22,21 +22,25 @@
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
-//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+
 char server[] = "protected-anchorage-54868.herokuapp.com";    // name address for Google (using DNS)
 
 // Set the static IP address to use if the DHCP fails to assign
-IPAddress ip(192, 168, 0, 177);
+IPAddress ip(192, 168, 0, 214);
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server
 // that you want to connect to (port 80 is default for HTTP):
 EthernetClient client;
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+const int buttonPin = 4;     // the number of the pushbutton pin
+const int ledPin =  7;      // the number of the LED pin
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
-int estado =0;
+int estado= 0;
+unsigned long lastConnectionTime = 0;             // last time you connected to the server, in milliseconds
+const unsigned long postingInterval = 10L * 1000L; // delay between updates, in milliseconds
+// the "L" is needed to use long type numbers
+
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
@@ -47,28 +51,17 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-
+   delay(1000);
   // start the Ethernet connection:
-  if (Ethernet.begin(mac) == 0) {
+   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
     // try to congifure using IP address instead of DHCP:
     Ethernet.begin(mac, ip);
   }
-  // give the Ethernet shield a second to initialize:
-  delay(1000);
-  Serial.println("connecting...");
-
-  // if you get a connection, report back via serial:
-
- 
-  if (client.connect(server, 80)) {
-    Serial.println("connected");
-  } else {
-    // if you didn't get a connection to the server:
-    Serial.println("connection failed");
-   }
-  
-}
+    // print the Ethernet board/shield's IP address:
+  Serial.print("My IP address: ");
+  Serial.println(Ethernet.localIP());
+ }
 
 void loop() {
   // read the state of the pushbutton value:
@@ -76,45 +69,52 @@ void loop() {
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
-    if (estado==1) {
-      Ethernet.begin(mac, ip);
-      delay(1000);
-    client.connect(server, 80);
-     
-
-     Serial.println("connecting...");
-     }
-    
-    // Make a HTTP request:
-    client.println("GET /test?led=ON HTTP/1.1");
-    client.println("Host: protected-anchorage-54868.herokuapp.com");
-    client.println("Connection: close");
-    client.println();
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-  } else {
+     digitalWrite(ledPin, HIGH);
+        estado=1;
+        Serial.print(estado);
+        } else {
     // turn LED off:
     digitalWrite(ledPin, LOW);
   }
+ 
   // if there are incoming bytes available
   // from the server, read them and print them:
   if (client.available()) {
     char c = client.read();
-    Serial.print(c);
+    Serial.println(c);
   }
+  // if ten seconds have passed since your last connection,
+  // then connect again and send data:
+  if (millis() - lastConnectionTime > postingInterval) {
+    httpRequest();
+  }
+}
 
-  
+// this method makes a HTTP connection to the server:
+void httpRequest() {
+  // close any connection before send a new request.
+  // This will free the socket on the WiFi shield
+  client.stop();
 
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting.");
-    estado=1;
-    Serial.print(estado);
-    client.stop();
-   
-    // do nothing forevermore:
-    while (true);
+  // if there's a successful connection:
+  if (client.connect(server, 80)) {
+    Serial.println("connecting...");
+    // send the HTTP GET request:
+    if(estado==1){
+       client.println("GET /test?led=ON HTTP/1.1");
+       estado=0;
+    }else{
+    client.println("GET /test?led=OFF HTTP/1.1");
+    }
+    client.println("Host: protected-anchorage-54868.herokuapp.com");
+    client.println("Connection: close");
+    client.println();
+
+    // note the time that the connection was made:
+    lastConnectionTime = millis();
+  } else {
+    // if you couldn't make a connection:
+    Serial.println("connection failed");
   }
 }
 
