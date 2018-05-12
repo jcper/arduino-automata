4461225d31f3dd9ba9e61abd66dafb9b0fac0de0
jcper <you@example.com>
2018-05-13 00:16:06 +0200

    funcionalidad display

--- WebClientheroku/WebClientheroku.ino
+++ WebClientheroku/WebClientheroku.ino
@@ -9,16 +9,32 @@
  modified 9 Apr 2012
  by Tom Igoe, based on work by Adrian McEwen
  Cliente para heruku basico sistema de alarma cada 10 segundos.
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 8
+ * LCD D6 pin to digital pin 9
+ * LCD D7 pin to digital pin 10
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
  */
 
 #include <SPI.h>
 #include <Ethernet.h>
 #include <string.h>
 #include <EEPROM.h>
+#include <LiquidCrystal.h>
 #include "DHT.h" 
 #define DHTPIN 2 
 #define DHTTYPE DHT11 
-DHT dht(DHTPIN, DHTTYPE); 
+DHT dht(DHTPIN, DHTTYPE);
+
+
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
 byte mac[6];
@@ -34,9 +50,10 @@ IPAddress ip(192, 168, 0, 214);
 // with the IP address and port of the server
 // that you want to connect to (port 80 is default for HTTP):
 EthernetClient client;
+const int rs =A0, en = 3, d4 = 5, d5 = 6, d6 = 8, d7 = 9; 
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 const int buttonPin = 4;     // the number of the pushbutton pin
 const int ledPin =  7;      // the number of the LED pin
-const int S0=5;
 String cabeceraGet="GET /test?";
 String id="mac=";
 String estat="&estado=";
@@ -70,6 +87,11 @@ struct MyStruct{
 };
 
 void setup() {
+   
+  // Print a message to the LCD.
+    
+   
+
     pinMode(ledPin, OUTPUT);
     pinMode(buttonPin, INPUT);
     urlserver="";
@@ -83,6 +105,8 @@ void setup() {
     comando=false;
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
+  lcd.begin(16, 2);
+  
    MyStruct customVar; //Variable to store custom object read from EEPROM.
   EEPROM.get(eeAddress, customVar);
 
@@ -103,13 +127,14 @@ void setup() {
   array_to_string(mac, 6, str);
   Serial.println(str);
   ID=str;
-
+  
   
   Serial.println(ID);
   Serial.println(server);
   Serial.println(tiempo_polling);
-  
-  
+  lcd.setCursor(0,0);
+  lcd.print("Leyendo Eprom");
+
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
   // initialize the pushbutton pin as an input:
@@ -127,13 +152,24 @@ void setup() {
     // print the Ethernet board/shield's IP address:
   Serial.print("My IP address: ");
   Serial.println(Ethernet.localIP());
-   
+  lcd.setCursor(0,1);
+  lcd.print("Inicio Sistema");
+  delay(3000);
+  lcd.clear();
+  
  }
 
 void loop() {
+  // (note: line 1 is the second row, since counting begins with 0):
+    
+  // print the number of seconds since reset:
+  
+     // set the cursor to column 0, line 1     
+     
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
- 
+  
+
   if(millis()-tiempoUltimaLectura>2000){    
    h = dht.readHumidity(); //se lee la humedad
    t= dht.readTemperature(); // se lee la temperatura
@@ -145,9 +181,16 @@ void loop() {
   if (buttonState == HIGH) {
         estado=1;
        Serial.println(" Alarma enviada desde arduino  " + ID);
-        delay(500);
+        lcd.setCursor(0,1);
+        lcd.print("                 ");
+        delay(100);
+        lcd.setCursor(0,1);
+        lcd.print("A[1]:"+ID);
         
-        } 
+        }
+
+       
+
   // if there are incoming bytes available
   // from the server, read them and print them:
   
@@ -162,15 +205,21 @@ void loop() {
           Serial.print(c);
           if(contador==23 && c=='r' && comando==false){
              comando=true;
-               
+             lcd.setCursor(0,1);
+             lcd.print("                 ");   
           }
           if(contador==27 && c=='0' && comando==true){
              digitalWrite(ledPin,LOW);
              salida=0;
+              
           }
            if(contador==27 && c=='1' && comando==true){
              digitalWrite(ledPin,HIGH);
              salida=1;
+            lcd.setCursor(0,1);
+            lcd.print("                 ");
+            lcd.setCursor(0,1);
+            lcd.print("R[1]:+"+ID);
           }
           contador ++;
        }
@@ -183,6 +232,36 @@ void loop() {
   if (millis() - lastConnectionTime > postingInterval) {
     httpRequest();
     }
+    if(estado==0 && salida==0 && comunicacion==1){
+       lcd.setCursor(0,0);
+       lcd.print("[C][1][A][0][R][0]");
+      
+       
+    }
+     if(estado==0 && salida==1 && comunicacion==1){
+       lcd.setCursor(0,0);
+       lcd.print("[C][1][A][0][R][1]");
+       
+    }
+    if(estado==1 && salida==1 && comunicacion==1){
+       lcd.setCursor(0,0);
+       lcd.print("[C][1][A][1][R][1]");
+       
+       
+    }
+
+    if(estado==1 && salida==0 && comunicacion==1){
+       lcd.setCursor(0,0);
+       lcd.print("[C][1][A][1][R][0]");
+       
+     }
+     if(comunicacion==0){
+       lcd.setCursor(0,0);
+       lcd.print("                  ");
+       lcd.setCursor(0,0);
+       lcd.print("fallo ethernet");
+     }
+  
   }
 
 
@@ -206,6 +285,8 @@ void httpRequest() {
     client.println("Connection: close");
     client.println();
     estado=0;
+    lcd.setCursor(0,1);
+    lcd.print("                 ");
     // note the time that the connection was made:
     lastConnectionTime = millis();
   } else {
